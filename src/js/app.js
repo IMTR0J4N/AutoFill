4733d38ebd6b0c9b5826ddadfd6a5541cf68567b
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2023-05-11 11:59:54 +0200

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,25 @@
 import '../css/style.css';
 import '../css/reset.css';
+
+const httpRequest = new XMLHttpRequest();
+const autoFillArea = document.getElementById('autofill--area');
+const suggestionList = document.getElementById('suggestion--list-item');
+
+httpRequest.open("GET", "../../assets/listeDeMot.txt");
+
+const showSuggestion = () => {
+
+    autoFillArea.oninput = () => {
+
+    }
+}
+
+const onSuggestionClick = () => {
+
+    const res = httpRequest.response.split("\n\r");
+
+
+    if (autoFillArea.ariaValueMax.length >= 2) {
+        const filtredSuggestion = res.filter(el => el.startsWith(autoFillArea.value)).slice(0, 10);
+    }
+}
